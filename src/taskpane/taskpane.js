833455ad272980ab8e9496d78cf97cd6bb95e0a3
jkras <jkrasnow@oakland.edu>
2023-12-14 20:18:12 -0500

    create table complete

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -7,30 +7,70 @@
 /*test*/
 Office.onReady((info) => {
   if (info.host === Office.HostType.Excel) {
+  // Assign event handlers and other initialization logic.
+  document.getElementById("create-table").onclick = () => tryCatch(createTable);
     document.getElementById("sideload-msg").style.display = "none";
     document.getElementById("app-body").style.display = "flex";
-    document.getElementById("run").onclick = run;
+  /*  document.getElementById("run").onclick = run; */
   }
 });
-
-export async function run() {
-  try {
+async function createTable() {
     await Excel.run(async (context) => {
-      /**
-       * Insert your Excel code here
-       */
-      const range = context.workbook.getSelectedRange();
 
-      // Read the range address
-      range.load("address");
+const currentWorksheet = context.workbook.worksheets.getActiveWorksheet();
+const expensesTable = currentWorksheet.tables.add("A1:D1", true /*hasHeaders*/);
+expensesTable.name = "ExpensesTable";
 
-      // Update the fill color
-      range.format.fill.color = "yellow";
+expensesTable.getHeaderRowRange().values =
+    [["Date", "Merchant", "Category", "Amount"]];
 
-      await context.sync();
-      console.log(`The range address was ${range.address}.`);
+expensesTable.rows.add(null /*add at the end*/, [
+    ["1/1/2017", "The Phone Company", "Communications", "120"],
+    ["1/2/2017", "Northwind Electric Cars", "Transportation", "142.33"],
+    ["1/5/2017", "Best For You Organics Company", "Groceries", "27.9"],
+    ["1/10/2017", "Coho Vineyard", "Restaurant", "33"],
+    ["1/11/2017", "Bellows College", "Education", "350.1"],
+    ["1/15/2017", "Trey Research", "Other", "135"],
+    ["1/15/2017", "Best For You Organics Company", "Groceries", "97.88"]
+]);
+
+
+expensesTable.columns.getItemAt(3).getRange().numberFormat = [['\u20AC#,##0.00']];
+expensesTable.getRange().format.autofitColumns();
+expensesTable.getRange().format.autofitRows();
+
+
+        await context.sync();
     });
-  } catch (error) {
-    console.error(error);
-  }
 }
+
+/** Default helper for invoking an action and handling errors. */
+async function tryCatch(callback) {
+    try {
+        await callback();
+    } catch (error) {
+        // Note: In a production add-in, you'd want to notify the user through your add-in's UI.
+        console.error(error);
+    }
+}
+//export async function run() {
+//  try {
+//    await Excel.run(async (context) => {
+//      /**
+//       * Insert your Excel code here
+//       */
+//      const range = context.workbook.getSelectedRange();
+//
+//      // Read the range address
+//      range.load("address");
+//
+//      // Update the fill color
+//      range.format.fill.color = "yellow";
+//
+//      await context.sync();
+//      console.log(`The range address was ${range.address}.`);
+//    });
+//  } catch (error) {
+//    console.error(error);
+//  }
+//}
